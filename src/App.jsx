0c7a8688a2a1f323d7a1e63f2f403044e9dba578
Bruno Torres <br.torres.del@gmail.com>
2025-03-01 18:49:15 -0300

    Adicionadas as fontes base + configuração do light / dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,19 @@ import './App.css';
 
 function App() {
 
+  let [prefTheme, setPrefTheme] = useState('');
+
+  function changeTheme() {
+    if (prefTheme) {
+      return setPrefTheme('')
+    }
+
+    return setPrefTheme('dark')
+  }
+
   return (
-    <div className="w-screen h-screen bg-slate-500 flex justify-center p6 pt-6">
+    <div className={`w-screen h-screen bg-back-light ${prefTheme} dark:bg-back-dark flex justify-center p6 pt-6`}>
+      <button onClick={changeTheme}>Troca</button>
       Hello World!
     </div>
   )
