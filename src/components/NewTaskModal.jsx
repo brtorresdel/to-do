0ac17bb25e91e0cf6b4118a423066807b6b651f8
Bruno Torres <br.torres.del@gmail.com>
2025-03-30 21:54:29 -0300

    Adicionadas novas formatações para o modal de nova tarefa + adição de cores faltantes ao CSS

--- src/components/NewTaskModal.jsx
+++ src/components/NewTaskModal.jsx
@@ -1,23 +1,36 @@
 import { X } from 'lucide-react'
 
+function DataInput({ name, label }) {
+    return <div className="flex flex-col gap-2 p-3">
+        <label 
+        className='text-lg'
+        htmlFor={name}>{label}</label>
+        <input 
+        className='bg-label-bg-light dark:bg-label-bg-dark rounded-md shadow-inner p-1'
+        type="text" name={name} id="" />
+    </div>
+}
+
 function NewTaskModal() {
     return <div className="w-full h-full bg-stone-950/50 absolute flex justify-center items-center" aria-modal='true'>
         <div className=" w-2xl h-2xl
         bg-task-light dark:bg-task-dark shadow-lg rounded-md">
-            <div className="bg-newTask-bar-light dark:bg-newTask-bar-dark rounded-tl-md rounded-tr-md flex flex-row justify-between items-center p-3">
-                <p>Nova tarefa</p>
-                <div>
-                    <button>Adicionar</button>
-                    <button><X /></button>
+            <div className="bg-newTask-bar-light dark:bg-newTask-bar-dark rounded-tl-md rounded-tr-md flex flex-row justify-between items-center p-3
+            font-noto-sans">
+                <p className='text-xl'>Nova tarefa</p>
+                <div className='flex flex-row gap-4'>
+                    <button className='bg-add-light dark:bg-add-dark text-font-light dark:text-font-dark p-2 rounded-md duration-300 hover:scale-105 active:scale-95 text-xl'>
+                        Adicionar
+                    </button>
+                    <button className='stroke-font-light dark:stroke-font-dark hover:scale-105 active:scale-95 duration-300'>
+                        <X className="size-9"/>
+                    </button>
                 </div>
             </div>
-            <div className="flex flex-col">
-                <label htmlFor="name">Nome da tarefa (obrigatório)</label>
-                <input type="text" name="name" id="" />
-                <label htmlFor="description">Descrição</label>
-                <input type="text" name="description" id="" />
-                <label htmlFor="deadline">Prazo</label>
-                <input type="text" name="deadline" id="" />
+            <div className='font-noto-sans'>
+                <DataInput name='taskName' label='Nome da Tarefa (obrigatório)'/>
+                <DataInput name='description' label='Descrição'/>
+                <DataInput name='deadline' label='Prazo'/>
                 <input type="text" placeholder='Observações'/>
             </div>
         </div>
