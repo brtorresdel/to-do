21d40160e2b246a6db486a428055128e6bde961b
Bruno Torres <br.torres.del@gmail.com>
2025-10-06 23:10:30 -0300

    Adicionadas as validações para envio de nova tarefa à lista

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -16,6 +16,8 @@ export default function TaskForm({ task, close, hidden }: { task?: Task, close: 
     const [description, setDescription] = useState("");
     const [observations, setObservations] = useState("");
 
+    const [submitTry, setSubmitTry] = useState(false);
+
     const { theme } : ThemeContextType = useContext(ThemeContext); 
 
     const taskFormCss: taskFormCss = {
@@ -40,6 +42,8 @@ export default function TaskForm({ task, close, hidden }: { task?: Task, close: 
 
     const newTask = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
+        setSubmitTry(true);
+        if (!title || !description) return;
         TaskLS.addTask({
             title: title,
             deadline: dueDate !== "" ? new Date(dueDate) : null,
@@ -47,12 +51,14 @@ export default function TaskForm({ task, close, hidden }: { task?: Task, close: 
             observations: observations !== "" ? observations : null,
             finished: false
         })
+        setSubmitTry(false);
         resetModal();
     }
 
     const editTask = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         TaskLS.updateTask({
+            id: task?.id,
             title: title,
             deadline: dueDate !== "" ? new Date(dueDate) : null,
             description: description,
@@ -63,45 +69,45 @@ export default function TaskForm({ task, close, hidden }: { task?: Task, close: 
     }
 
     return (
-        hidden ? (
-            <Modal>
-                <div className={`w-1/2 h-fit rounded-md opacity-100 p-5 flex flex-col shadow-mg gap-2 ${taskFormCss.bg}`}>
-            <div className="flex flex-row flex-nowrap justify-between w-full items-center">
-                <h1 className={`text-2xl font-bold ${taskFormCss.text}`}>
-                    {!task ? "Nova tarefa" : "Editar tarefa"}
-                </h1>
-                <div>
-                    <X className="hover:scale-130 transform duration-150" onClick={resetModal} color={btnCss.svgColor}/>
-                    <span className="absolute -top-7 left-1/2 -translate-x-1/2 bg-gray-800 text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none z-10">
-                        Fechar
-                    </span>
+        hidden ? <Modal>
+            <div className={`w-1/2 h-fit rounded-md opacity-100 p-5 flex flex-col shadow-mg gap-2 ${taskFormCss.bg}`}>
+                <div className="flex flex-row flex-nowrap justify-between w-full items-center">
+                    <h1 className={`text-2xl font-bold ${taskFormCss.text}`}>
+                        {!task ? "Nova tarefa" : "Editar tarefa"}
+                    </h1>
+                    <div>
+                        <X className="hover:scale-130 transform duration-150" onClick={resetModal} color={btnCss.svgColor}/>
+                        <span className="absolute -top-7 left-1/2 -translate-x-1/2 bg-gray-800 text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none z-10">
+                            Fechar
+                        </span>
+                    </div>
                 </div>
+                <form className="w-full flex flex-col gap-2">
+                    <InputElem name="título *" required={true} theme={theme} value={title} onChange={value => setTitle(value)} submitTry={submitTry}/>
+                    <InputElem name="prazo" required={false}  theme={theme} value={dueDate} onChange={value => setDueDate(value)} submitTry={submitTry}/>
+                    <InputElem name="descrição *" required={true} theme={theme} value={description} onChange={value => setDescription(value)} submitTry={submitTry}/>
+                    <InputElem name="observações" required={false} theme={theme} value={observations} onChange={value => setObservations(value)} submitTry={submitTry}/>
+                    <div>
+                        <button 
+                        className={`${btnCss.bg} h-auto w-full p-3 rounded-lg flex flex-row justify-center-safe gap-2
+                        hover:scale-102 hover:drop-shadow-xl active:scale-95 transition duration-150`}
+                        onClick={task ? editTask : newTask}>
+                            <span className={btnCss.textColor}>{task ? "Editar Tarefa" : "Adicionar tarefa"}</span> <Plus color={btnCss.svgColor}/>
+                        </button>
+                       </div>
+                </form>
             </div>
-            <form className="w-full flex flex-col gap-2">
-                <InputElem name="título" required={true} theme={theme} value={title} onChange={(value) => setTitle(value)}/>
-                <InputElem name="prazo" required={false} theme={theme} value={dueDate} onChange={value => setDueDate(value)}/>
-                <InputElem name="descrição" required={true} theme={theme} value={description} onChange={value => setDescription(value)}/>
-                <InputElem name="observações" required={false} theme={theme} value={observations} onChange={value => setObservations(value)}/>
-                <div>
-                    <button 
-                    className={`${btnCss.bg} h-auto w-full p-3 rounded-lg flex flex-row justify-center-safe gap-2
-                    hover:scale-102 hover:drop-shadow-xl active:scale-95 transition duration-150`}
-                    onClick={newTask}>
-                        <span className={btnCss.textColor}>Adicionar tarefa</span> <Plus color={btnCss.svgColor}/>
-                    </button>
-                </div>
-            </form>
-        </div>
-            </Modal>
-        ) : (<></>)
+        </Modal> : (<></>)
     )
 }
 
-function InputElem ( {name, required, value, theme, onChange}: {name: string, required: boolean, value?: string, theme: "light" | "dark", onChange: (value: string) => void} ) {
+function InputElem ( {name, required, value, theme, submitTry, onChange}: {name: string, required: boolean, value?: string, theme: "light" | "dark", submitTry: boolean, onChange: (value: string) => void} ) {
     // TODO: add the required function to validade the data entry
     
     const capitalize = name.charAt(0).toUpperCase() + name.slice(1);
     const textArea = name === "descrição" || name === "observações";
+
+    const showRequired = required && submitTry && (!value || value.trim() === "");
 
     const inputCss: inputCss = {
         bg: theme === 'light' ? "bg-bg-light" : "bg-bg-dark",
@@ -111,14 +117,14 @@ function InputElem ( {name, required, value, theme, onChange}: {name: string, re
 
     return <div className="w-full mb-1 mt-1">
         <div className="flex flex-row gap-2 items-center">
-            <label htmlFor={name} className={`font-semibold ${inputCss.text}`}>{required ? capitalize + " *" : capitalize}</label>
-            {required && <span className="text-red-400 text-sm hidden">Este campo é obrigatório!</span>}
+            <label htmlFor={name} 
+            className={`font-semibold ${inputCss.text}`}>{capitalize}</label>
+            {required && <span className={`text-red-400 font-bold text-sm ${showRequired ? "" : "hidden"}`}>Este campo é obrigatório!</span>}
         </div>
         {textArea ?
             <textarea
                 name={name}
                 placeholder={capitalize}
-                required={required}
                 className={`w-full border-1 m-0.25 ${inputCss.border} ${inputCss.text} ${inputCss.bg} rounded-lg p-1.5 focus:border-2 focus:m-0 transition duration-200 h-30 overflow-y-auto align-top`}
                 value={value ? value : ""}
                 onChange={e => onChange(e.target.value)}
